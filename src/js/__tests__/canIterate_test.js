df94f1d4013548905e81a7ee7c85b89063d4ac42
Alyona <alena.bentley.aston@gmail.com>
2025-07-05 03:07:29 +0300

    Test fixed

--- src/js/__tests__/canIterate_test.js
+++ src/js/__tests__/canIterate_test.js
@@ -2,16 +2,17 @@ import { canIterate } from "../canIterate";
 
 describe('Tests for the canIterate() function', () => {
     
-    test('function returns true', () => {
-        expect(canIterate(new Map())).toBe(true);
-        expect(canIterate(new Set())).toBe(true);
-        expect(canIterate("Netology")).toBe(true);
-        expect(canIterate([1,2,3])).toBe(true);
-    })
+    const expected = [
+        { item: canIterate(new Map()), result: true },
+        { item: canIterate(new Set()), result: true },
+        { item: canIterate("Netology"), result: true },
+        { item: canIterate([1,2,3]), result: true },
+        { item: canIterate(null), result: false },
+        { item: canIterate(undefined), result: false },
+        { item: canIterate(12), result: false },
+    ];
 
-    test('function returns false', () => {
-        expect(canIterate(null)).toBe(false);
-        expect(canIterate(undefined)).toBe(false);
-        expect(canIterate(12)).toBe(false);
+    test.each(expected)('returns $result for input', ({ item, result }) => {
+        expect(item).toBe(result);
     })
 })
